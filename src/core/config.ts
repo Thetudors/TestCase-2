72ff74e5ec838798d5e3b3b03e91d51583520b38
Onat Aktaş <onat_akts@hotmail.com>
2025-03-08 15:23:26 +0300

    fix: update asset paths for consistency and build configuration

--- src/core/config.ts
+++ src/core/config.ts
@@ -6,22 +6,22 @@ export const SAFESIZE: GameSize = { width: 800, height: 600 };
 
 
 export const SPRITES: spriteConfig[] = [
-    { alias: "buttonbg", src: "/assets/sprites/button_bg.png" },
-    { alias: "card-0", src: "/assets/sprites/card_0.png" },
-    { alias: "card-1", src: "/assets/sprites/card_1.png" },
-    { alias: "sad", src: "/assets/sprites/sad.png" },
-    { alias: "intrigued", src: "/assets/sprites/intrigued.png" },
-    { alias: "satisfied", src: "/assets/sprites/satisfied.png" },
-    { alias: "neutral", src: "/assets/sprites/neutral.png" },
-    { alias: "laughing", src: "/assets/sprites/laughing.png" },
-    { alias: "Sheldon", src: "/assets/sprites/Sheldon.png" },
-    { alias: "Penny", src: "/assets/sprites/Penny.png" },
-    { alias: "Leonard", src: "/assets/sprites/Leonard.png" },
-    { alias: "rect", src: "/assets/sprites/rect.png" },
-    { alias: "fire", src: "/assets/sprites/fire.png" },
+    { alias: "buttonbg", src: "./assets/sprites/button_bg.png" },
+    { alias: "card-0", src: "./assets/sprites/card_0.png" },
+    { alias: "card-1", src: "./assets/sprites/card_1.png" },
+    { alias: "sad", src: "./assets/sprites/sad.png" },
+    { alias: "intrigued", src: "./assets/sprites/intrigued.png" },
+    { alias: "satisfied", src: "./assets/sprites/satisfied.png" },
+    { alias: "neutral", src: "./assets/sprites/neutral.png" },
+    { alias: "laughing", src: "./assets/sprites/laughing.png" },
+    { alias: "Sheldon", src: "./assets/sprites/Sheldon.png" },
+    { alias: "Penny", src: "./assets/sprites/Penny.png" },
+    { alias: "Leonard", src: "./assets/sprites/Leonard.png" },
+    { alias: "rect", src: "./assets/sprites/rect.png" },
+    { alias: "fire", src: "./assets/sprites/fire.png" },
 
 ]
 
 export const FONTS: FontFace[] = [
-    new FontFace('sniglet-regular', 'url(assets/fonts/Sniglet-Regular.ttf)')
+    new FontFace('sniglet-regular', 'url(./assets/fonts/Sniglet-Regular.ttf)')
 ];
--- src/core/sound/config.ts
+++ src/core/sound/config.ts
@@ -3,17 +3,17 @@ import { SoundChannel, SoundConfig } from "./types";
 export const SOUNDS: SoundConfig[] = [
     {
         alias: "EffectCardMove",
-        url: "assets/sounds/card_move.mp3",
+        url: "./assets/sounds/card_move.mp3",
         soundChannel: SoundChannel.EFFECT,
     },
     {
         alias: "EffectFire",
-        url: "assets/sounds/fire.mp3",
+        url: "./assets/sounds/fire.mp3",
         soundChannel: SoundChannel.EFFECT,
     },
     {
         alias: "EffectPopup",
-        url: "assets/sounds/popup.mp3",
+        url: "./assets/sounds/popup.mp3",
         soundChannel: SoundChannel.EFFECT,
     }
 ];
